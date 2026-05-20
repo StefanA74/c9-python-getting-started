a312199d89cdc06a0201eaee612648982239a87d
StefanA74 <stefan.althoff@ortec.com>
2026-05-20 15:44:18 +0200

    New lessons added

---
+++ python-for-beginners/xx - My own lessons/11_String operations.py
@@ -0,0 +1,7 @@
+output1 = 'Hello, {}, {}'.format('Alice', 'Bob')
+print(output1)
+
+first_name = 'Alice'
+last_name = 'Smith'
+output2 = 'Hello, {}, {}'.format(first_name, last_name)
+print(output2)
