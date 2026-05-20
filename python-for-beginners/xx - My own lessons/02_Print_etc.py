666b62a7f09a4b950e30aa31cfe95ad8be6160a6
StefanA74 <stefan.althoff@ortec.com>
2026-05-20 15:11:57 +0200

    Update

---
+++ python-for-beginners/xx - My own lessons/02_Print_etc.py
@@ -0,0 +1,14 @@
+
+# A simple print statement
+print ('Hello World')
+# Break line with \n
+print ('Very long \n sentence')
+# Get user input and store it in variable name
+name = input('What is your name? ')
+print ('Hello ' + name)
+print ('Start calculation...')
+x = 12 + 24
+print ('The result is ' + str(x))
+y = x / 0
+print ('The result is ' + str(y))
+
